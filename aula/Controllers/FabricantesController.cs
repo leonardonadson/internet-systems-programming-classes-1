1a9d32a8d70a4e169a5dbd1b4f82db66642f3313
Leonardo Nadson <leonardo.nom@outlook.com>
2022-06-02 11:41:15 -0300

    aula.02.06.2022

--- aula/Controllers/FabricantesController.cs
+++ aula/Controllers/FabricantesController.cs
@@ -1,4 +1,5 @@
 ﻿using aula.App_Start.Models;
+using aula.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,40 @@ namespace aula.Controllers
     public class FabricantesController : Controller
     {
         private EFContext context = new EFContext();
+
+        private static IList<Fabricante> fabricantes = new List<Fabricante>()
+        {
+        new Fabricante() { FabricanteId = 1, Nome = "LG"},
+        new Fabricante() { FabricanteId = 1, Nome = "Microsoft"}
+        };
+
         // GET: Fabricantes
         public ActionResult Index()
         {
-            return View(context.Fabricantes.OrderBy(c => c.Nome));
+            return View(
+                fabricantes
+                //context.Fabricantes.OrderBy(c => c.Nome)
+                );
         }
+
+        // GET: Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            fabricantes.Add(fabricante);
+            fabricante.FabricanteId = fabricantes.Select(m => m.FabricanteId).Max() + 1;
+            //context.Fabricantes.Add(fabricante);
+            //context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
